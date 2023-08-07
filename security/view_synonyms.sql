ba8fe62c6a82b65088099e87338d1a16f854e2c8
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-07 14:10:39 -0500

    Update view_synonyms.sql

--- security/view_synonyms.sql
+++ security/view_synonyms.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/security/view_synonyms.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Creates synonyms in the current schema for all views in the specified schema.
 -- Call Syntax  : @view_synonyms (schema-name)
 -- Last Modified: 28/01/2001
