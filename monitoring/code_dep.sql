d74bd30153a601537fe79d1d27a7566d6dcd8833
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-07 14:18:14 -0500

    Update code_dep.sql

--- monitoring/code_dep.sql
+++ monitoring/code_dep.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/code_dep.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays all dependencies of specified object.
 -- Call Syntax  : @code_dep (schema-name or all) (object-name)
 -- Last Modified: 15/07/2000
