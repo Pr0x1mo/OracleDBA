1fdd10685fa52ee96c3c3d5e9c4ccf7baf713a9d
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-08 13:16:17 -0500

    Update index_monitoring_status.sql

--- monitoring/index_monitoring_status.sql
+++ monitoring/index_monitoring_status.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/index_monitoring_status.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Shows the monitoring status for the specified table indexes.
 -- Call Syntax  : @index_monitoring_status (table-name) (index-name or all)
 -- Last Modified: 04/02/2005
