e4b87c20b687aaec3d0036853890bff7bcbd1718
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 15:31:59 -0500

    Update pipes.sql

--- resource_manager/monitoring/pipes.sql
+++ resource_manager/monitoring/pipes.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/pipes.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays a list of all database pipes.
 -- Requirements : Access to the V$ views.
 -- Call Syntax  : @pipes
