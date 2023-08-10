1c7474619642af2a91f594d3c64e7202e4ab287a
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 07:16:58 -0500

    Update logfiles.sql

--- monitoring/logfiles.sql
+++ monitoring/logfiles.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/logfiles.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays information about redo log files.
 -- Requirements : Access to the V$ views.
 -- Call Syntax  : @logfiles
