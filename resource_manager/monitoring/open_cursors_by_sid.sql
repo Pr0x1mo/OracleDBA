2d1f8e97ecf04414f961c34bc4900c014f3436a2
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 15:12:10 -0500

    Update open_cursors_by_sid.sql

--- resource_manager/monitoring/open_cursors_by_sid.sql
+++ resource_manager/monitoring/open_cursors_by_sid.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/open_cursors_by_sid.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays the SQL statement held for a specific SID.
 -- Comments     : The SID can be found by running session.sql or top_session.sql.
 -- Requirements : Access to the V$ views.
