8bf8d8e1f7a9390179d76ccfe9a661abc4f6177c
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 08:05:13 -0500

    Update pdb_spfiles.sql

--- 12c/pdb_spfiles.sql
+++ 12c/pdb_spfiles.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/12c/pdb_spfiles.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays information from the pdb_spfile$ table.
 -- Requirements : Access to pdb_spfile$ and v$pdbs.
 -- Call Syntax  : @pdb_spfiles
