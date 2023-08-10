1a342fd25cb6809de63faca73f45b2443a4af671
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 11:36:17 -0500

    Update users_with_role.sql

--- monitoring/users_with_role.sql
+++ monitoring/users_with_role.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/users_with_role.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays a list of users granted the specified role.
 -- Requirements : Access to the DBA views.
 -- Call Syntax  : @user_with_role DBA
