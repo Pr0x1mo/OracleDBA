a9fc7f10b556c5d4bf6c3e9eb7c0639ab1995034
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 09:39:37 -0500

    Update trigger_ddl.sql

--- script_creation/trigger_ddl.sql
+++ script_creation/trigger_ddl.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/script_creation/trigger_ddl.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Creates the DDL for specified trigger, or all trigger.
 -- Call Syntax  : @trigger_ddl (schema) (trigger-name or all)
 -- Last Modified: 16/03/2013 - Rewritten to use DBMS_METADATA
