01fc287d36b69f8496c640a26bbc86509a188615
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 15:39:37 -0500

    Update session_pga.sql

--- resource_manager/monitoring/session_pga.sql
+++ resource_manager/monitoring/session_pga.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/session_pga.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays information about PGA usage for each session.
 -- Requirements : Access to the V$ views.
 -- Call Syntax  : @session_pga
