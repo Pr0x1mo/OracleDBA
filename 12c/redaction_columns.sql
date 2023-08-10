4c435c2bc5290d0290ca66213530b8be67d1cf2d
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 08:06:21 -0500

    Update redaction_columns.sql

--- 12c/redaction_columns.sql
+++ 12c/redaction_columns.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/12c/redaction_columns.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays information about columns related to redaction policies.
 -- Requirements : Access to the REDACTION_COLUMNS view.
 -- Call Syntax  : @redaction_columns (schema | all) (object | all)
