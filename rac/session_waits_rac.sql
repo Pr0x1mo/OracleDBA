e113701f1f81608f2a0f14b6fccada5adde5c26b
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 08:26:48 -0500

    Update session_waits_rac.sql

--- rac/session_waits_rac.sql
+++ rac/session_waits_rac.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/10g/session_waits_rac.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays information on all database session waits for the whole RAC.
 -- Requirements : Access to the V$ views.
 -- Call Syntax  : @session_waits_rac
