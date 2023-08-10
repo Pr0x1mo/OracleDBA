5f08565cfa58f6101f7c92ac3c56f657484f0ff9
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 15:32:16 -0500

    Update profiler_runs.sql

--- resource_manager/monitoring/profiler_runs.sql
+++ resource_manager/monitoring/profiler_runs.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/profiler_runs.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays information on all profiler_runs.
 -- Requirements : Access to the V$ views.
 -- Call Syntax  : @profiler_runs.sql
