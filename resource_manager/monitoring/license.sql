65ae0bcad1a349b36d2d267e251b78740c0a6974
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 11:58:08 -0500

    Update license.sql

--- resource_manager/monitoring/license.sql
+++ resource_manager/monitoring/license.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/license.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays session usage for licensing purposes.
 -- Requirements : Access to the V$ views.
 -- Call Syntax  : @license
