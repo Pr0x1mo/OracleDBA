bbc13d2a132c28359aa64e3889455f1753656f69
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 09:45:34 -0500

    Update event_histogram.sql

--- 10g/event_histogram.sql
+++ 10g/event_histogram.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/10g/event_histogram.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays histogram of the event waits times.
 -- Requirements : Access to the V$ views.
 -- Call Syntax  : @event_histogram "(event-name)"
