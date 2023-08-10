79844215b167872d5aeb1b7ee90d46a0918fcdff
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 11:57:54 -0500

    Update latches.sql

--- resource_manager/monitoring/latches.sql
+++ resource_manager/monitoring/latches.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/latches.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays information about all current latches.
 -- Requirements : Access to the V$ views.
 -- Call Syntax  : @latches
