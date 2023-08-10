1b6c374b9b11ca56555b0655b701b1c33b86d14d
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 15:48:40 -0500

    Update tablespaces.sql

--- resource_manager/monitoring/tablespaces.sql
+++ resource_manager/monitoring/tablespaces.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/tablespaces.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays information about tablespaces.
 -- Requirements : Access to the DBA views.
 -- Call Syntax  : @tablespaces
