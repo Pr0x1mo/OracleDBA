6ed62f51ce84dc6c885424ead81503c8349443d4
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 15:12:29 -0500

    Update options.sql

--- resource_manager/monitoring/options.sql
+++ resource_manager/monitoring/options.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/options.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays information about all database options.
 -- Requirements : Access to the V$ views.
 -- Call Syntax  : @options
