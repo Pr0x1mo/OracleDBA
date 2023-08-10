181824b00daec88388d41c45c78703a8245064e2
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 11:40:54 -0500

    Update code_dep_distinct.sql

--- resource_manager/monitoring/code_dep_distinct.sql
+++ resource_manager/monitoring/code_dep_distinct.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/code_dep_distinct.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays a tree of dependencies of specified object.
 -- Call Syntax  : @code_dep_distinct (schema-name) (object-name) (object_type or all)
 -- Last Modified: 15/07/2000
