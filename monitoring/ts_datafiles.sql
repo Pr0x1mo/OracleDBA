7702da8438465ba85879321169c7f81108388e92
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 11:14:27 -0500

    Update ts_datafiles.sql

--- monitoring/ts_datafiles.sql
+++ monitoring/ts_datafiles.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/ts_datafiles.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays information about datafiles for the specified tablespace.
 -- Requirements : Access to the V$ views.
 -- Call Syntax  : @ts_datafiles (tablespace-name)
