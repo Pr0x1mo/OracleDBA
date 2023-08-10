e23d8090902ab410f32d2b1fefd0ced2061f6ab4
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 15:20:02 -0500

    Update user_sessions.sql

--- resource_manager/monitoring/user_sessions.sql
+++ resource_manager/monitoring/user_sessions.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/user_sessions.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays information on all user database sessions.
 -- Requirements : Access to the V$ views.
 -- Call Syntax  : @user_sessions
