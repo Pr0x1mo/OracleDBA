75c97b970859f4462d05a0af59e6205020b0e057
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 15:30:46 -0500

    Update trace_run_details.sql

--- resource_manager/monitoring/trace_run_details.sql
+++ resource_manager/monitoring/trace_run_details.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/trace_run_details.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays details of a specified trace run.
 -- Requirements : Access to the V$ views.
 -- Call Syntax  : @trace_run_details.sql (runid)
