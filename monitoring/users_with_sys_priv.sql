5aa483558ae5c0bd9e12fd2ea28ecea1ec7af75a
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 11:36:24 -0500

    Update users_with_sys_priv.sql

--- monitoring/users_with_sys_priv.sql
+++ monitoring/users_with_sys_priv.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/users_with_sys_priv.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays a list of users granted the specified role.
 -- Requirements : Access to the DBA views.
 -- Call Syntax  : @users_with_sys_priv "UNLIMITED TABLESPACE"
