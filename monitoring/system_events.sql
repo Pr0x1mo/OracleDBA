bca57d128b6b1c6260c3eac0e52b6a5bc4ef8ab1
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 11:02:55 -0500

    Update system_events.sql

--- monitoring/system_events.sql
+++ monitoring/system_events.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/system_events.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays information on all system events.
 -- Requirements : Access to the V$ views.
 -- Call Syntax  : @system_events
