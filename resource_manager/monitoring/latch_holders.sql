21c4db1091bc3770dac3b9b69062b0b66f48e413
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 11:57:33 -0500

    Update latch_holders.sql

--- resource_manager/monitoring/latch_holders.sql
+++ resource_manager/monitoring/latch_holders.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/latch_holders.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays information about all current latch holders.
 -- Requirements : Access to the V$ views.
 -- Call Syntax  : @latch_holders
