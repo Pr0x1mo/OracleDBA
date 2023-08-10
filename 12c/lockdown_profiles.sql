3284b3f954306b5cfc1a263bc0d446d5b1c7628f
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 08:02:40 -0500

    Update lockdown_profiles.sql

--- 12c/lockdown_profiles.sql
+++ 12c/lockdown_profiles.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/12c/lockdown_profiles.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays information about lockdown profiles.
 -- Requirements : Access to the DBA views.
 -- Call Syntax  : @lockdown_profiles
