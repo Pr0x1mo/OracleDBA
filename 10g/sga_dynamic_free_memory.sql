1f750d397fcb689b07322b7d46e212e2d548d628
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 10:04:27 -0500

    Update sga_dynamic_free_memory.sql

--- 10g/sga_dynamic_free_memory.sql
+++ 10g/sga_dynamic_free_memory.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/10g/sga_dynamic_free_memory.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Provides information about free memory in the SGA.
 -- Requirements : Access to the v$ views.
 -- Call Syntax  : @sga_dynamic_free_memory
