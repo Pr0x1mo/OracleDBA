3be2d6a346c5b75dd3ec4a51fa958f3a4426adbb
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 15:15:06 -0500

    Update part_tables.sql

--- resource_manager/monitoring/part_tables.sql
+++ resource_manager/monitoring/part_tables.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/part_tables.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays information about all partitioned tables.
 -- Requirements : Access to the DBA views.
 -- Call Syntax  : @part_tables
