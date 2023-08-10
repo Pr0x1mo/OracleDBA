cea614aa85e802f5aef61d54fb1e416c1200fa28
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 08:13:30 -0500

    Update stop-node-manager.sh

--- weblogic/stop-node-manager.sh
+++ weblogic/stop-node-manager.sh
@@ -1,6 +1,5 @@
-# -----------------------------------------------------------------------------------
-# File Name    : https://oracle-base.com/dba/weblogic/stop-node-manager.sh
-# Author       : Tim Hall
+-- -----------------------------------------------------------------------------------
+-- Author       : Pr0x1mo
 # Description  : Stops the WebLogic Node Manager by killing the processes.
 # Call Syntax  : ./stop-node-manager.sh
 # Last Modified: 15-JUN-2013
