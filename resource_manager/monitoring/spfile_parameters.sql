7f9c930bb36603e02924bac79beb078c33f2110e
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 15:45:01 -0500

    Update spfile_parameters.sql

--- resource_manager/monitoring/spfile_parameters.sql
+++ resource_manager/monitoring/spfile_parameters.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/spfile_parameters.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays a list of all the spfile parameters.
 -- Requirements : Access to the v$ views.
 -- Call Syntax  : @spfile_parameters
