d59e81efebc01d1a9e8ae4d4fa0105bb3e36b79d
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 09:58:17 -0500

    Update job_programs.sql

--- 10g/job_programs.sql
+++ 10g/job_programs.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/10g/job_programs.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays scheduler information about job programs.
 -- Requirements : Access to the DBA views.
 -- Call Syntax  : @job_programs
