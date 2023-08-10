45a212952bf69a32db00ded960277451406d486e
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2023-08-10 10:31:22 -0500

    Update objects.sql

--- monitoring/objects.sql
+++ monitoring/objects.sql
@@ -1,6 +1,5 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/objects.sql
--- Author       : Tim Hall
+-- Author       : Pr0x1mo
 -- Description  : Displays information about all database objects.
 -- Requirements : Access to the dba_objects view.
 -- Call Syntax  : @objects [ object-name | % (for all)]
